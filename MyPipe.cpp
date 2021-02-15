9d4e1f7c0a0048fb239c9b2f6a7ce3323c60482f
Giorgi Sharmiashvili <sharmiko123@gmail.com>
2021-02-15 15:20:24 +0400

    finish my pipe

--- MyPipe.cpp
+++ MyPipe.cpp
@@ -37,21 +37,23 @@ public:
     int readData(int &buff)
     {
         auto bytes = ::read(this->input, &buff, INPUT_SIZE * sizeof(int)) / sizeof(int);
-
         return (int) bytes;
     }
 
     int writeData(int &buff)
     {
-		auto size = ::write(this->output, &buff, INPUT_SIZE * sizeof(int)) / sizeof(int);
-
-		return (int) size;
+	    auto size = ::write(this->output, &buff, INPUT_SIZE * sizeof(int)) / sizeof(int);
+	    return (int) size;
     }
 };
 
 
 void get_user_input(std::string message_prompt, short &choice)
 {
+    /*
+     *  Helper function for displaying message prompot to user 
+     *  and getting input from 
+     */
     std::cout << message_prompt << std::endl;
     std::cout << "#: ";
     std::cin >> choice;
@@ -64,43 +66,90 @@ void get_user_input(std::string message_prompt, short &choice)
 }
 
 
-void write_data(MyPipe &myPipe, int input_type, std::string process_name)
+void read_data(MyPipe &myPipe, int input_type, std::string process_name)
 {
+    /*  
+     *  Helper function for reading data either from console or file
+     *  
+     *  Paramters:
+     *      &myPipe (MyPipe): Pipe in which processes communicate
+     *      input_type (int): type of input, tells from where to read data
+     *                        examples:
+     *                          1 - read data from console and write it to pipe
+     *                          2 - read data from file and write it to pipe
+     *      process_name (std::string): name of process which is performing certain task 
+     */
     std::cout << "Process (" << process_name << ") ";
-    std::cout << "Enter " << INPUT_SIZE << " numbers:" << std::endl;
-
-    int buff[INPUT_SIZE];
-    for (int i = 0; i < INPUT_SIZE; ++i)
-    {
-        std::cin >> buff[i];
-    }
 
     if (input_type == 1)
     {
+        std::cout << "Enter " << INPUT_SIZE << " numbers:" << std::endl;
+        int buff[INPUT_SIZE];
+        for (int i = 0; i < INPUT_SIZE; ++i)
+        {
+            std::cin >> buff[i];
+        }
         myPipe.writeData(*buff);
     }
     else
     {
-        std::ofstream file(OUTPUT_FILE);
-        for (int i = 0; i < INPUT_SIZE; ++i)
+        std::cout << "reading data from file" << std::endl;
+        std::ifstream file(INPUT_FILE);
+        std::string text;
+        int buff[INPUT_SIZE];
+        int i = 0;
+        while (std::getline(file, text))
         {
-            file << buff[i];
+            for (char c : text)
+            {
+
+                if (isdigit(c))
+                {
+                    buff[i] = (int) c - 48;
+                    i += 1;
+                    if (i == INPUT_SIZE)
+                    {
+                        break;
+                    }
+                }
+            }
         }
+        myPipe.writeData(*buff);
     }
 }
 
-void read_data(MyPipe &myPipe, int output_type, std::string process_name)
+void write_data(MyPipe &myPipe, int output_type, std::string process_name)
 {
+    /*
+     * Helper function for displaying data either in console or writing it into the file
+     * 
+     *  Paramters:
+     *      &myPipe (MyPipe): Pipe in which processes communicate
+     *      input_type (int): type of input, tells where to write data
+     *                        examples:
+     *                          1 - read data from pipe and display it into console
+     *                          2 - read data from pipe and write it into file
+     *      process_name (std::string): name of process which is performing certain task 
+     * 
+     */
     int buffer[INPUT_SIZE];
     myPipe.readData(*buffer);
-    
-    std::cout << "Process (" << process_name << ") ";
-
     if (output_type == 1)
     {
+        std::cout << "Process (" << process_name << ") displaying data to console" << std::endl;
         for (int i = 0; i < INPUT_SIZE; ++i)
         {
             std::cout << buffer[i] << " ";
+        }
+        std::cout << std::endl;
+    }
+    else
+    {
+        std::cout << "Process (" << process_name << ") writing data to file" << std::endl;
+        std::ofstream file(OUTPUT_FILE);
+        for (int i = 0; i < INPUT_SIZE; ++i)
+        {
+            file << buffer[i] << " ";
         }
     }
 }
@@ -108,10 +157,10 @@ void read_data(MyPipe &myPipe, int output_type, std::string process_name)
 
 int main()
 {
-
     short child_process;
     short input_type;
     short output_type;
+    
     get_user_input("Choose child process: child a or child b? (1 for a, 2 for b)", child_process);
     get_user_input("Choose input type: (1 for console, 2 for file)", input_type);
     get_user_input("Choose output type: (1 for console, 2 for file)", output_type);
@@ -123,63 +172,42 @@ int main()
     decltype(fork()) process_b;
     int status;
 
-    process_a = fork();
+    (process_a = fork()) && (process_b = fork());
+
     if (process_a == 0)
     {
-        // Child process a
+        // child a
         if (child_process == 1)
         {
-            // write data
-            std::cout << "A writing\n"; 
-            write_data(*myPipe, input_type, "a");
+            // read data 
+            read_data(*myPipe, input_type, "a");
         }
         else
+        {
+            // write data
+            while(waitpid(process_b, &status, 0) != -1);
+            write_data(*myPipe, output_type, "a");
+        }
+    }
+    else if (process_b == 0)
+    {
+        // child b
+        if (child_process == 2)
         {
             // read data
-            std::cout << "A reading data\n";
-            read_data(*myPipe, output_type, "a");
-        }
-    }
-    else if (process_a == -1)
-    {
-        // error occurred
-        std::cerr << "Could not create process: Error!" << std::endl;
-        std::exit(-1);
-    }
-    else 
-    {
-        // Parent process
-        wait(nullptr);
-
-        process_b = fork();
-        if (process_b == 0)
-        {
-            // Child process b
-            if (child_process == 2)
-            {
-                // write data
-                std::cout << "B writing\n";
-                write_data(*myPipe, input_type, "b");
-            }
-            else
-            {
-                // read data
-                std::cout << "B reading\n";
-                read_data(*myPipe, output_type, "b");
-            }
-        }
-        else if (process_b == -1)
-        {
-            // error occurred
-            std::cerr << "Could not create process: Error!" << std::endl;
-            std::exit(-1);
+            read_data(*myPipe, input_type, "b");
         }
         else
         {
-            // Parent process
-            wait(nullptr);
+            // write data
+            while(waitpid(process_a, &status, 0) != -1);
+            write_data(*myPipe, output_type, "b");
         }
     }
+    else
+    {
+        // parent code 
+        wait(nullptr);
+    }
 }
-
 
