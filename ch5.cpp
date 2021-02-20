750d42881b5042ac5ab5f795a4358897463741e8
Giorgi Sharmiashvili <sharmiko123@gmail.com>
2021-02-20 16:18:39 +0400

    add chapter 5

--- ch5.cpp
+++ ch5.cpp
@@ -1,9 +1,31 @@
 #include <unistd.h>
 #include <iostream>
+#include <sys/shm.h>
 #include <sys/wait.h>
 
 #include <array>
 #include <string_view>
+
+
+char *get_shared_memory()
+{
+    auto key = ftok("myfile", 42);
+    auto shm = shmget(key, 0x1000, 0666 | IPC_CREAT);
+
+    return static_cast<char *>(shmat(shm, nullptr, 0));
+}
+
+void mysystem(const char *command)
+{
+    if (fork() == 0)
+    {
+        execlp(command, command, nullptr);
+    }
+    else{
+        wait(nullptr);
+    }
+}
+
 
 int data = 0;
 
@@ -42,6 +64,13 @@ public:
     {
         ::write(m_handles.at(1), msg.data(), msg.size());
     }
+
+    void redirect()
+    {
+        dup2(m_handles.at(1), STDOUT_FILENO);
+        close(m_handles.at(0));
+        close(m_handles.at(1));
+    }
 };
 
 int main(void)
@@ -52,15 +81,29 @@ int main(void)
     {
         sleep(1);
         std::cout << "Parent\n";
+        //auto msg = get_shared_memory();
+        //msg[0] = 42;
+        //p.write("done");
 
-        p.write("done");
-        wait(NULL);
+
+        wait(nullptr);
+        std::cout << p.read() << std::endl;
     }
     else
     {
-        auto msg = p.read();
+        //auto msg = p.read();
+        //auto msg1 = get_shared_memory();
+        //while (msg1[0] != 42);
+
+        p.redirect();
+        execlp("ls", "ls", nullptr);
 
         std::cout << "child\n";
-        std::cout << "msg: " << msg << std::endl;
+        //std::cout << "msg: " << msg1 << std::endl;
     }
+
+    
+    //execl("/bin/ls", "ls", nullptr);
+    std::cout << "Hello WWorld\n";
+
 }
