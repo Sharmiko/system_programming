8c9d14b3babeb69ad10105c3a8c3f1fd44b4a9bc
Giorgi Sharmiashvili <sharmiko123@gmail.com>
2021-03-07 16:51:45 +0400

    update shared memory class

--- MyShMemory.cpp
+++ MyShMemory.cpp
@@ -9,13 +9,6 @@
 
 #include <iostream>
 
-// size of shared memory
-#define N 100
-
-// random numbers range
-#define RANDOM_MIN 100
-#define RANDOM_MAX 200
-
 
 /*
  * Custom class for managing shared memory 
@@ -24,6 +17,7 @@ class MyShMemory
 {
 private:
     key_t key;
+    int memory_size;
     /* Helper private function used to get shared
      * memory id. If some kind of error occures 
      * program is termianted.
@@ -31,12 +25,12 @@ private:
     int get_shared_memory_id()
     {
         // get shared memory id
-        int shm_id = shmget(this->key, N * sizeof(int), IPC_CREAT | IPC_EXCL | 0660);
+        int shm_id = shmget(this->key, this->memory_size, IPC_CREAT | IPC_EXCL | 0660);
         if (shm_id == -1) // check error cases
         {
             if (errno == EEXIST) //  if id already exists try to get existing shared memory id.
             {
-                shm_id = shmget(this->key, N * sizeof(int), 0);
+                shm_id = shmget(this->key, this->memory_size, 0);
                 if (shm_id == -1)
                 {
                     std::cerr << "Error: Could not get identifier for existed shared memory." << std::endl;
@@ -57,8 +51,9 @@ public:
      *  Contructor for MyShMemory class. This constructor creates IPC key 
      *  using pathname. In case of error program is terminated
      */
-    MyShMemory()
+    MyShMemory(int memory_size)
     {
+        this->memory_size = memory_size;
         this->key = ftok("MyShMemory.cpp", 1);
         if (this->key == -1)
         {
@@ -71,10 +66,10 @@ public:
      * Function that attaches shared memory to calling process and
      * returns pointer to memory space.
      */
-    int *get_shared_memory()
+    void *get_shared_memory()
     {
         int shm_id = this->get_shared_memory_id();
-        int *memory = (int*) shmat(shm_id, NULL, 0);
+        void *memory = (void*) shmat(shm_id, NULL, 0);
         if (memory == (int*)(-1))
         {
             std::cerr << "Error: Could not attach shared memory to calling process." << std::endl;
@@ -87,7 +82,7 @@ public:
     /*
      * Function that detaches shared memory form current process. 
      */ 
-    void detach_shared_memory(int *memory)
+    void detach_shared_memory(void *memory)
     {
         if (shmdt(memory))
         {
@@ -96,76 +91,3 @@ public:
         }
     }
 };
-
-/*
- * Program 1:
- *  Parent process creates 2 child process and shared memory for 100 
- *  integer values.
- *  First child initializes shared memory with random numbers in range [100, 200].
- *  Second Child using same shared memory calculates how many values are multiples
- *  of 5.
- */
-void program_one()
-{
-    MyShMemory *shm = new MyShMemory();
-
-    decltype(fork()) process_a;
-    decltype(fork()) process_b;
-
-    // create 2 child processes
-    (process_a = fork()) && (process_b = fork());
-
-    if (process_a == 0)
-    {
-        // child a
-        int *memory = shm->get_shared_memory();
-
-        srand(time(nullptr));
-        for (int i = 0; i < N; ++i)
-        {
-            memory[i] = random() % (RAND_MAX - RANDOM_MIN + 1) + RANDOM_MIN;
-        }
-
-        // detach memory
-        shm->detach_shared_memory(memory);
-    }
-    else if (process_b == 0)
-    {
-        // child b
-        int *memory = shm->get_shared_memory();
-
-        // wait untill data is available
-        while(!memory[0]);
-
-        // count multiples of 5
-        int count = 0;
-        for (int i = 0; i < N; ++i)
-        {
-            if (memory[i] % 5 == 0)
-            {
-                ++count;
-            }
-        }
-
-        std::cout << "There are " << count << " multiples of 5." << std::endl;
-        shm->detach_shared_memory(memory);
-    }
-    else
-    {
-        // parent
-        wait(nullptr);
-    }
-}
-
-int main()
-{   
-
-    // execute program one
-    std::cout << "Executing program one..." << std::endl;
-    program_one();
-
-
-    // execute program two
-
-    return 0;
-}
