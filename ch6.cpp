90464f4d95f7944d241b999f2990b1bc8faf8b7d
Giorgi Sharmiashvili <sharmiko123@gmail.com>
2021-05-01 22:01:55 +0400

    add chapter 6

--- ch6.cpp
+++ ch6.cpp
@@ -4,6 +4,64 @@
 #include <iostream>
 
 
+#ifndef DEBUG_LEVEL 
+#define DEBUG_LEVEL 0
+#endif
+
+#ifndef NDEBUG
+#define DEBUG(fmt, args...) \
+    fprintf(stdout, "%s [%d]: " fmt, __FILE__, __LINE__, args);
+#else 
+#define DEBUG(...)
+#endif 
+
+
+#ifdef DEBUG_LEVEL
+constexpr auto g_debug_level = DEBUG_LEVEL;
+#else 
+constexpr auto g_debug_level = 0;
+#endif 
+
+#ifdef NDEBUG
+constexpr auto g_ndebug = true;
+#else 
+constexpr auto g_ndebug = false;
+#endif
+
+
+#define console std::cout << __FILE__ << " [" << __LINE__ << "]: "
+
+template<std::size_t LEVEL>
+const void debug(void(*func)())
+{
+    if constexpr (!g_ndebug && (LEVEL <= g_debug_level))
+    {
+        std::cout << "\033[1;32mDEBUG\033[0m ";
+        func();
+    };
+}
+
+template<std::size_t LEVEL>
+const void warning(void(*func)())
+{
+    if constexpr (!g_ndebug && (LEVEL <= g_debug_level))
+    {
+        std::cout << "\033[1;33mWARNING\033[0m ";
+        func();
+    };
+}
+
+template<std::size_t LEVEL>
+const void fatal(void(*func)())
+{
+    if constexpr (!g_ndebug && (LEVEL <= g_debug_level))
+    {
+        std::cout << "\033[1;31mFATAL ERROR\033[0m ";
+        func();
+        std::exit(-1);
+    };
+}
+
 class custom_errno {};
 
 
@@ -78,6 +136,35 @@ int main()
 
     buf_t<2> buf2;
     std::cin >> buf2;
+
+    DEBUG("The answer is: %d\n", 42);
+    DEBUG("The answer is: %d\n", 43);
+
+    if constexpr (!g_ndebug && (0 <= g_debug_level))
+    {
+        std::cout << "The answer is: " << 44 << "\n";
+    }
+
+        if constexpr (!g_ndebug && (1 <= g_debug_level))
+    {
+        std::cout << "The answer is: " << 45 << "\n";
+    }
+
+    debug<0>([] {
+        console << "The answer is: " << 46 << "\n";
+    });
+
+    debug<1>([] {
+        console << "The answer is: " << 47 << "\n";
+    });
+
+    warning<0>([] {
+        console << "The answer might be: " << 47 << "\n";
+    });
+
+    fatal<0>([] {
+        console << "The answer was not: " << 48 << "\n";
+    });
 
     return 0;
 }
